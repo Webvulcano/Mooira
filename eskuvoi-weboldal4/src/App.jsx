7219a6d2feb32e0141bb88b8c444bcf0cb625a3a
lehel200 <lehel200@gmail.com>
2026-04-21 08:27:00 +0200

    fix

--- eskuvoi-weboldal4/src/App.jsx
+++ eskuvoi-weboldal4/src/App.jsx
@@ -14,7 +14,7 @@ import Footer from './components/Footer'
 import FontTweaks from './components/FontTweaks'
 import CopyToast from './components/CopyToast'
 import { SpeedInsights } from "@vercel/speed-insights/react"
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App() {
   return (
